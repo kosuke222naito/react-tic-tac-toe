5a5c8b36abbbad61442a9bfffb774b63dde32e99
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-03-15 10:25:14 +0900

    implement tic tac toe tutorial

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Board from "./Board";
+
+const Game = () => {
+  const historyInit = [Array(9).fill("")];
+  const [history, setHistory] = useState(historyInit);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  const handlePlay = (nextSquares: string[]) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  const jumpTo = (nextMove: number) => {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((_, move: number) => {
+    let description = "Go to ";
+    description += move > 0 ? "move #" + move : "game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
---
+++ src/Square.tsx
@@ -0,0 +1,14 @@
+type SquareProps = {
+  value: string;
+  onSquareClick: () => void;
+};
+
+const Square = ({ value, onSquareClick }: SquareProps) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
